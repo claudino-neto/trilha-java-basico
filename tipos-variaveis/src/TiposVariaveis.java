550c9461ced0be33c0a402e3830836b523ffbcba
Claudino Neto <115763601+Zeromil2@users.noreply.github.com>
2024-08-29 12:23:26 -0300

    estudos de tipos primitivos de variáveis no java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //tipos primitivos
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 2104058963;
+        long cpf = 14058464598L;
+        float pi = 3.14F;
+        double salarioMinimo = 2500.30;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // usando casting
+
+        final double VALOR_DE_PI = 3.14; // criando constantes
+
+        char letra = 'a';
+
+        boolean verdade = true;
+
+        String nome = "Claudino";
     }
 }
